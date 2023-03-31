1340042860cbad04b9bb9d6c9c26a4adc9e32f0b
halotukozak <kozak.bart.miej@gmail.com>
2023-03-31 17:02:43 +0200

    identifiers exercise is done

--- ids_template.c
+++ ids_template.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
 #include <stdbool.h>
 
@@ -14,7 +12,7 @@
 #define MAX_IDS 1024
 
 #define MAX_LINE 128
-#define NEW_LINE '\n'
+#define BLANK_CHARS 16
 
 
 char identifiers[MAX_IDS][MAX_ID_LEN];
@@ -33,75 +31,104 @@ const char *keywords[] = {
 
 const int keywords_size = 32;
 
-bool isBlank(char c) {
-    return c == ' ' || c == '\t' || c == NEW_LINE;
+bool isValidCharacter(char c) {
+    return (c >= 48 && c <= 57) || (c >= 65 && c <= 90) || (c >= 97 && c <= 122) || c == 95 || c == 35;
 }
 
-bool isLetter(char c) {
-    return (c >= 65 && c <= 90) || (c >= 97 && c <= 122) || c == 95;
+bool isValidFirstCharacter(char c) {
+    return (c >= 65 && c <= 90) || (c >= 97 && c <= 122) || c == 95 || c == 35;
 }
 
-
-bool isKeyword(const char id[]) {
+bool isNotKeyword(const char id[]) {
     for (int i = 0; i < keywords_size; ++i)
-        if (strcmp(keywords[i], id) == 0) return true;
-    return false;
+        if (strcmp(keywords[i], id) == 0) return false;
+    return true;
 }
 
-bool isInIdentifiers(char id[]) {
+bool isNotInIdentifiers(const char id[]) {
     for (int i = 0; i < lastId; ++i)
-        if (strcmp(identifiers[i], id) == 0) return true;
-    return false;
+        if (strcmp(identifiers[i], id) == 0) return false;
+    return true;
 }
 
-void parseWord(char word[MAX_ID_LEN], int len) {
-    if (isLetter(word[0])) {
-        char id[MAX_ID_LEN];
-        for (int i = 0; i < len; ++i) {
-            if (word[i] == '(') break;
-            id[i] = word[i];
-        }
-        if (isKeyword(id) || isInIdentifiers(id)) return;
-        strcpy(identifiers[lastId++], id);
-    }
+void parseWord(const char word[MAX_ID_LEN]) {
+    if (isValidFirstCharacter(word[0]) && isNotKeyword(word) & isNotInIdentifiers(word))
+        strcpy(identifiers[lastId++], word);
 }
 
 int find_idents() {
     char string[MAX_LINE];
+    bool during_word = false;
+    bool omit = false;
+    bool during_multiline_comment = false;
+    bool during_string = false;
+
+    char string_char = '"';
     while (fgets(string, MAX_LINE, stdin) != NULL) {
-        bool during_word = false;
-        bool during_comment = false;
-        char word[MAX_ID_LEN];
+        char word[MAX_ID_LEN] = {0};
         int lastLetter = 0;
-        for (int i = 0; i < strlen(string); ++i) {
+        size_t len = strlen(string);
+        for (int i = 0; i < len; ++i) {
             char c = string[i];
-            if (c == '/') {
-                if (string[i + 1] == '*') during_comment = true;
-                break;
+
+            if (!during_multiline_comment) {
+                // comments
+                if (i + 1 < len) {
+                    // start of the multiline comment
+                    if (c == '/' && string[i + 1] == '*') {
+                        during_multiline_comment = true;
+                        omit = true;
+                        i += 1;
+                    }
+                        // one-line comment
+                    else if (c == '/' && string[i + 1] == '/') break;
+                }
+
+                if (!omit) {
+                    // start of the string
+                    if (i > 0 && (c == '"' || c == '\'') && string[i - 1] != '\\') {
+                        string_char = c;
+                        during_string = true;
+                        omit = true;
+                    }
+
+                    // start of the word
+                    if (isValidFirstCharacter(c)) {
+                        during_word = true;
+                        word[lastLetter++] = c;
+                    } else if (during_word && isValidCharacter(c)) {
+                        word[lastLetter++] = c;
+                    }
+
+                    // end of the word
+                    if (during_word && !isValidCharacter(c)) {
+                        parseWord(word);
+                        lastLetter = 0;
+                        memset(word, 0, MAX_ID_LEN);
+                        during_word = false;
+                        omit = false;
+                    }
+                } else if (during_string && c == string_char) {
+                    if ((i > 0 && string[i - 1] != '\\') ||
+                        (i > 1 && (string[i - 1] != '\\' || (string[i - 1] == '\\' && string[i - 2] == '\\')))) {
+
+                        omit = false;
+                        during_string = false;
+                    }
+                }
+            } else if (i + 1 < len && c == '*' && string[i + 1] == '/') {
+                during_multiline_comment = false;
+                omit = false;
+                i += 1;
             }
-            if (c == '*' && string[i + 1] == '/') during_comment = false;
-            if (!during_comment && during_word && isBlank(c)) {
-                parseWord(word, lastLetter);
-                lastLetter = 0;
-                during_word = false;
-            }
-            if (!isBlank(c)) {
-                during_word = true;
-                word[lastLetter++] = c;
-            }
-        }
-        if (!during_comment && during_word) {
-            parseWord(word, lastLetter);
-            during_word = false;
         }
     }
 
+//    for (int i = 0; i < lastId; ++i) printf("%s\n", identifiers[i]);
     return lastId;
 }
-
 
 int main(void) {
     printf("%d\n", find_idents());
     return 0;
 }
-
